c359469b59ce7f67c0decb1b4f8908c7ba43de29
unknown <shehrysoftologics@gmail.com>
2023-07-28 04:33:23 +0500

    new v

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -15,7 +15,7 @@
 
 
         <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="/">
                 <i class="typcn typcn-device-desktop menu-icon"></i>
                 <span class="menu-title">Dashboard</span>
                 <div class="badge badge-danger">new</div>
